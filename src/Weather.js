aa44e7f2a54776a538a15ba09aa886de578b5451
lornabicaldo <69823972+lornabicaldo@users.noreply.github.com>
2021-01-13 19:31:12 +0100

    Added API Integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactAnimatedWeather from 'react-animated-weather';
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-    return( 
+  
+  const [weatherData, setWeatherData] = useState({ready: false});
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      pressure: response.data.main.pressure,
+    });
+  }
+
+  if (weatherData.ready) {
+   return( 
     <div className="weather">
        <div className="weather-icon">
            <ReactAnimatedWeather
@@ -13,33 +29,42 @@ export default function Weather() {
          animate= 'true'
          />
        </div>
-       <h2 >32<span className="unit">°C |°F</span></h2>
-       <h3 >Valleta</h3>
-       <h4 >Sunny</h4>
+       <h2 >{Math.round(weatherData.temperature)}<span className="unit">°C |°F</span></h2>
+       <h3 >{weatherData.city}</h3>
+       <h4 className="text-capitalized">{weatherData.description}</h4>
        <h5>Sun, Jan. 10, 2021</h5>
        <div className="row row-cols-1 row-cols-md-3 g-0">
      <div className="col">
     <div className="card">
         <ul>
-          <li>Wind🌀:78km/p</li>
+          <li>Wind🌀:{Math.round(weatherData.wind)}km/h</li>
         </ul>
     </div>
   </div>
   <div className="col">
     <div className="card">
         <ul>
-          <li>Humidity💧:89%</li>
+          <li>Humidity💧:{weatherData.humidity}%</li>
         </ul>
     </div>
   </div>
   <div className="col">
     <div className="card">
         <ul>
-          <li>Pressure⏲:1022mb</li>
+          <li>Pressure⏲:{weatherData.pressure}</li>
         </ul>
     </div>
   </div>
   </div>
     </div>
-    );
+  );
+  } else {
+  const apiKey = "8faad2c6827a8ece72c2320ecccefbdb";
+  let city = `Valletta`;
+  let units = `metric`;
+  let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(handleResponse);
+
+  return "Loading...";
+  }
 }
